5d549e5af5b0d3ef99116806b4babdc716eda9fc
Fan, Xing Hua <xinghua.fan@siemens-energy.com>
2026-05-25 13:54:55 +0800

    first commit

---
+++ BlazorAccessControl.Interface/Model/IUser.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorAccessControl.Interface
+{
+    public interface IUser
+    {
+        public string Id { get; }
+
+        public string UserName { get;}
+
+        public string DisplayName { get; }
+
+        public string Email { get; }
+
+        public ICollection<IUserRole> UserRoles { get; }
+    }
+}
