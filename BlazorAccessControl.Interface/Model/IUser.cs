30b5a10845a2da4d2b58b644225f6b428afffd86
Fan, Xing Hua <xinghua.fan@siemens-energy.com>
2026-05-25 16:18:25 +0800

    add setter

--- BlazorAccessControl.Interface/Model/IUser.cs
+++ BlazorAccessControl.Interface/Model/IUser.cs
@@ -2,14 +2,14 @@
 {
     public interface IUser
     {
-        public string Id { get; }
+        public string Id { get; set; }
 
-        public string UserName { get;}
+        public string UserName { get; set; }
 
-        public string DisplayName { get; }
+        public string DisplayName { get; set; }
 
-        public string Email { get; }
+        public string Email { get; set; }
 
-        public ICollection<IUserRole> UserRoles { get; }
+        public ICollection<IUserRole> UserRoles { get; set; }
     }
 }
