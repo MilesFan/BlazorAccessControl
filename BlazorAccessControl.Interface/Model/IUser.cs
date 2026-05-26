5b7e10e829227ec84cd929f4919dfb5e300d73ea
Fan, Xing Hua <xinghua.fan@siemens-energy.com>
2026-05-26 17:39:52 +0800

    UserName must be nullable

--- BlazorAccessControl.Interface/Model/IUser.cs
+++ BlazorAccessControl.Interface/Model/IUser.cs
@@ -4,12 +4,14 @@
     {
         public string Id { get; set; }
 
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
 
-        public string DisplayName { get; set; }
+        public string? DisplayName { get; set; }
 
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
-        public ICollection<IUserRole> UserRoles { get; set; }
+        public ICollection<IRole> GetRoles();
+
+        public void SetRoles(ICollection<IRole> roles);
     }
 }
