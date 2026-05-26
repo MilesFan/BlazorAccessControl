b1d64d60b8ad034205d5265a03968683264d7d32
Fan, Xing Hua <xinghua.fan@siemens-energy.com>
2026-05-26 17:40:30 +0800

    add more methods

--- BlazorAccessControl.Interface/Service/IUserService.cs
+++ BlazorAccessControl.Interface/Service/IUserService.cs
@@ -7,23 +7,21 @@ namespace BlazorAccessControl.Interface
     public interface IUserService
     {
         public IUser? CurrentUser { get; }
-        
         public Task<ICollection<IRole>> GetAllRolesAsync();
-
         public Task<ICollection<IUser>> GetAllUsersAsync();
-
         public Task<IUser?> GetUserByIdAsync(string id);
-        public Task<ICollection<IRole>> GetRolesAsync(IUser user);
-
+        public Task<ICollection<IRole>> GetUserRolesAsync(IUser user);
+        public Task<IRole?> GetRoleByIdAsync(string id);
         public Task CreateUserAsync(IUser user);
-
         public Task UpdateUserAsync(IUser user);
-
+        public Task CreateRoleAsync(IRole role);
+        public Task UpdateRoleAsync(IRole role);
         public Task SetPasswordAsync(string id, string Password);
-
         public Task DeleteUserByIdAsync(string id);
-        
+        public Task DeleteRoleByIdAsync(string id);
         public Task SignInAsync(IUser user);
         public Task SignOutAsync(IUser user);
+        public Task ChangePasswordAsync(IUser user, string oldPassword, string newPassword);
+        public Task ResetPasswordAsync(IUser user, string newPassword);
     }
 }
