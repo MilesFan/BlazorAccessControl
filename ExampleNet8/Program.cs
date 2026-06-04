b1bcd7485592dcb3c55c821e273ff05b247bacdb
Fan, Xing Hua <xinghua.fan@siemens-energy.com>
2026-06-04 15:15:36 +0800

    add net8 example

---
+++ ExampleNet8/Program.cs
@@ -0,0 +1,75 @@
+using BlazorAccessControl.EFCore;
+using BlazorAccessControl.Interface;
+using ExampleNet8.Components;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExampleNet8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            
+            builder.Services
+                .AddCascadingAuthenticationState()
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+                })
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+                    options =>
+                    {
+                        options.LoginPath = "/account/signin";
+                        options.LogoutPath = "/account/signout";
+                        options.ForwardSignIn = "/account/signin";
+                    })
+                .AddIdentityCookies(option => {
+                    option.ApplicationCookie?.Configure(s => {
+                        s.LoginPath = "/account/signin";
+                        s.LogoutPath = "/account/signout";
+                        s.AccessDeniedPath ="/access-denied";
+                    });
+                });
+
+
+            builder.Services.AddDbContextFactory<DBContext>(lifetime: ServiceLifetime.Scoped);
+
+            builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<ApplicationRole>()
+                .AddEntityFrameworkStores<DBContext>()
+                .AddSignInManager()
+                .AddDefaultTokenProviders();
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddScoped<IUserService, DummyUserService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
